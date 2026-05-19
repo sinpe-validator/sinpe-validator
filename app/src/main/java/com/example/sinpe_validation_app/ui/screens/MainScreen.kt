f5873476a3974298f516194120cdda39291419dc
Angel Barboza Reyes <Angelbarbozareyes29@gmail.com>
2026-05-18 22:41:34 -0600

    quite la screen de ordenes

--- app/src/main/java/com/example/sinpe_validation_app/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/sinpe_validation_app/ui/screens/MainScreen.kt
@@ -1,57 +1,17 @@
 package com.example.sinpe_validation_app.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Mail
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MainScreen() {
-    var selectedTab by remember { mutableIntStateOf(0) }
-
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                NavigationBarItem(
-                    selected = selectedTab == 0,
-                    onClick = { selectedTab = 0 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Mail,
-                            contentDescription = "SMS"
-                        )
-                    },
-                    label = { Text("SMS") }
-                )
-
-                NavigationBarItem(
-                    selected = selectedTab == 1,
-                    onClick = { selectedTab = 1 },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = "Órdenes"
-                        )
-                    },
-                    label = { Text("Órdenes") }
-                )
-            }
-        }
-    ) { paddingValues ->
-        when (selectedTab) {
-            0 -> InboxScreen()
-            1 -> OrdersScreen(modifier = Modifier.padding(paddingValues))
+    Scaffold { paddingValues ->
+        // Usamos un Box para aplicar el padding y que InboxScreen no tire error
+        Box(modifier = Modifier.padding(paddingValues)) {
+            InboxScreen()
         }
     }
 }
